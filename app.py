185442372930aec33f15b28761f4f6e44387e72e
Bryan Haryanto <z5590944@ad.unsw.edu.au>
2026-05-19 10:35:52 +1000

    adding user model, register and login requests

--- app.py
+++ app.py
@@ -1,6 +1,38 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.py import User
 
 app = Flask(__name__)
+db = [] #temp
+
+@app.route("/register", methods=["POST"])
+def register():
+    data = request.get_json()
+    user = User(
+        id=data.get("id"),
+        name=data.get("name"),
+        password=data.get("password"),
+        age=data.get("age"),
+    )
+    db.append(user)
+    return jsonify({"message": "User registered successfully!"})
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    user = User(
+        id=data.get("id"),
+        name=data.get("name"),
+        password=data.get("password"),
+        age=data.get("age"),
+        gender=data.get("gender")
+    )
+    # Add login logic here
+    for u in db:
+        if u.name == user.name and u.password == user.password:
+            return user.id
+            return redirect(url_for("home"))
+    return jsonify({"message": "Invalid credentials!"}), 401
+
+
 
 @app.route("/")
 def home():
