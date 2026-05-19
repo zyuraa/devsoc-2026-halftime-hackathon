ed3797f4f10eb6f084ca0767cab81eed0e03d258
Adit Mahidadia <AditMahidadia@student.unsw.edu.au>
2026-05-19 16:43:01 +1000

    added tests

--- app.py
+++ app.py
@@ -66,7 +66,8 @@ def getUserInfo(id):
 # user searches for gyms
 @app.route("/<id>/groups", methods=["GET"])
 # user searches for groups
-# input: gym name, time range
+# input: gym name
+# e, time range
 # output: list of groups matching the criteria
 def search_groups(id):
     gym_name = request.args.get("gym_name")
@@ -82,6 +83,7 @@ def search_groups(id):
 
 @app.route("/<id>/groups", methods=["POST"])
 def create_group():
+    # user creates a group
     data = request.get_json()
     # Add group creation logic here
     group = Group(
@@ -99,7 +101,6 @@ def create_group():
             
         
     return jsonify({"message": "Gym not found!"}), 404
-# user creates a group
 
 @app.route("/<id>/groups/<group_id>", methods=["PUT"])
 # user joins a group
